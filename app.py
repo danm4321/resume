ab24e1cb2e0589755bf94c15f5bd08cd22d5e22d
danm4321 <89867392+danm4321@users.noreply.github.com>
2022-10-23 08:33:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ profile_pic = current_dir / "assets" / "profile-pic.png"
 
 
 # --- GENERAL SETTINGS ---
-PAGE_TITLE = "Digital CV | John Doe"
+PAGE_TITLE = "Digital CV | Dan Doe"
 PAGE_ICON = ":wave:"
 NAME = "Dan McC"
 DESCRIPTION = """
